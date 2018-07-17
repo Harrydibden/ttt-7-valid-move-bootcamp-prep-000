ef58298b4383746f096292491b978c260dcf26a3
Harry Dibden <harry-dibden@hotmail.com>
2018-07-17 08:14:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,9 @@
 def valid_move?(board, index)
   if index < 0 || index > 8
     false
-  elsif position_taken?(board, index) == true
-    true
+  elsif position_taken?(board, index) == false
+    false
+  else true
   end
 end
 
